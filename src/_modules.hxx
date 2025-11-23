1e8f0886129840f5392b047d99f7d0ca9c47f477
Subhajit Sahu <wolfram77@gmail.com>
2025-11-23 21:54:46 +0530

    :bug: with Graph.hxx

---
+++ src/_modules.hxx
@@ -0,0 +1,11 @@
+// Copyright (C) 2025 Subhajit Sahu
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// See LICENSE for full terms
+#pragma once
+
+// This is for C++20 modules.
+#ifdef _MODULE_
+#define GVE_EXPORT export
+#else
+#define GVE_EXPORT
+#endif
---
+++ src/main.ixx
@@ -0,0 +1,5 @@
+module;
+export module gve;
+
+#define EXPORT export
+#include "Graph.hxx"
